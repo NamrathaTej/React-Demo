89de9955d0544d1c01026666972b49481e801c35
NamrathaTej <namrata.gkr20@gmail.com>
2022-02-16 10:47:21 +0100

    search update

--- Documents/Namratha Assignment/React-project2/my-app/src/component/Home.js
+++ Documents/Namratha Assignment/React-project2/my-app/src/component/Home.js
@@ -6,27 +6,40 @@ import Search from "./Search";
 
 function Home() {
   const [countries, setCountries] = useState([]);
+  const[filteredData, setFilteredData] = useState([]);
   const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((response) => response.json())
-      .then((data) => setCountries(data));
-  }, []);
+      .then((data) => {
+          setCountries(data)
+          setFilteredData(data)
+      }
+   );
+ }, []);
 
   const handleChange = (event) => {
     setKeyword(event.target.value);
 
-    let filteredData= countries.filter((country)=>{
-        return country.name.common.toLowerCase().Search(keyword.toLowerCase())
-    }
-    )
+    let filteredData = countries.filter((country) => {
+      return (
+        country.name.common
+        .toLowerCase()
+        .search(keyword.toLocaleLowerCase()) !== -1
+      );
+    });
+    setFilteredData(filteredData)
   };
 
   return (
     <div>
-        <Search handleChange={handleChange} value={keyword}/>
-      <Countrytable countries={countries} />;
+      <Search
+        handleChange={handleChange}
+        value={keyword}
+        countries={countries}
+      />
+      <Countrytable countries={filteredData} />;
     </div>
   );
 }
